3be1106a3a469451585de74880e7f1898c775835
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-18 14:00:31 +0100

    Initial Commit

---
+++ NSUbiquitousKeyValueStoreSandbox/NSUbiquitousKeyValueStoreSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSUbiquitousKeyValueStoreSandboxApp.swift
+//  NSUbiquitousKeyValueStoreSandbox
+//
+//  Created by Zaid Neurothrone on 2022-12-18.
+//
+
+import SwiftUI
+
+@main
+struct NSUbiquitousKeyValueStoreSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
